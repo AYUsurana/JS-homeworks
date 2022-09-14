9b15e5c35aadd54e859ea0054190e400a7b9299d
Ayusheeva Surana <surana.ayusheeva98@gmail.com>
2022-09-14 19:49:53 +0300

    Tasks 3.1, 3.2

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,12 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
+  // Мой код
+  if ((arr1.length === arr2.length) && (arr1.every((item, i) => item === arr2[i]))) {
+    result = true;
+  } else {
+    result = false;
+  }
 
   return result; // boolean
 }
@@ -9,7 +14,8 @@ function compareArrays(arr1, arr2) {
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  // Мой код
+  resultArr = arr.filter((item) => item > 0).filter((item) => item % 3 === 0).map((item) => item * 10);
 
   return resultArr; // array
 }
