46f4f3f8d97e718a59027da4ef1906ee19f0dde9
Ayusheeva Surana <surana.ayusheeva98@gmail.com>
2022-09-15 22:22:04 +0300

    Task 4.1

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,46 @@
 function Student(name, gender, age) {
-    // Ваш код
+    // мой код
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  //мой код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+// мой код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [];
+  } 
+    
+  this.marks.push(mark);
+}
+
+Student.prototype.addMarks = function (...marks) {
+  if (this.marks === undefined) {
+    this.marks = [];
+  }
+
+  for (let i = 0; i < marks.length; i++) {
+    this.marks.push(marks[i]);
+  }
+}
+
+Student.prototype.getAverage = function() {
+  let summ = this.marks.reduce((m, s) => {
+   return  m + s;
+  }, 0);
+
+  let arith = summ/this.marks.length;
+  return (arith);
+}
+
+Student.prototype.exclude = function (reason) {
+  this.excluded = reason;
+  delete this.subject;
+  delete this.marks;
+}
