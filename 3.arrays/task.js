1a72b5ebf0042c10e0cd48b0bbbf036f74b3036a
Ayu Sun <51070212+AYUsurana@users.noreply.github.com>
2022-09-16 10:42:36 +0300

    Update task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -2,20 +2,13 @@ function compareArrays(arr1, arr2) {
   let result;
 
   // Мой код
-  if ((arr1.length === arr2.length) && (arr1.every((item, i) => item === arr2[i]))) {
-    result = true;
-  } else {
-    result = false;
-  }
-
-  return result; // boolean
+  return result (arr1.length === arr2.length) && (arr1.every((item, i) => item === arr2[i])) // boolean
 }
 
 function advancedFilter(arr) {
   let resultArr;
 
   // Мой код
-  resultArr = arr.filter((item) => item > 0).filter((item) => item % 3 === 0).map((item) => item * 10);
+  return resultArr = arr.filter((item) => item > 0).filter((item) => item % 3 === 0).map((item) => item * 10); // array
 
-  return resultArr; // array
 }
